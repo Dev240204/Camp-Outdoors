0ad55e5f4ea8b1221b4203c6a986da281c1efb4c
Dev <dev.ace21@sot.pdpu.ac.in>
2022-12-29 12:16:25 +0530

    updtated

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
+const { title } = require('process');
+const Campground = require('./models/campground');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(()=>{
+        console.log("Data base Connection Open!!");
+    })
+    .catch(()=>{
+        console.log("Error in Connecting Data Base");
+    })
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
 app.get('/',(req,res)=>{
     res.render('home');
+})
+app.get('/campground',async (req,res)=>{
+    const camp = new Campground({title : 'BackYard Camping'})
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000,()=>{
