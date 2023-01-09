2f54ef21c5a20262e23fe522db599dfa2ed244c1
Dev <dev.ace21@sot.pdpu.ac.in>
2023-01-09 17:48:34 +0530

    Updated seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDb = async() =>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random400 = Math.floor(Math.random()*400);
+        const price = Math.floor(Math.random()*10000)+1000;
         const camp = new Campground({
             location : `${cities[random400].city},${cities[random400].state}`,
             title : `${sample(descriptors)} ${sample(places)}`,
+            description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate enim, ex, similique, maxime sunt ullam aperiam quo alias hic ut quam libero ducimus rem et? Perferendis aliquam voluptates consequatur atque!',
+            image : 'https://source.unsplash.com/collection/483251',
+            price,
         })
         await camp.save();
     }
