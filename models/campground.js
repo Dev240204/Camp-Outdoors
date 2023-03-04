0076fce7d21c4f88ccd3db888e1266c68e8820c1
Dev <dev.ace21@sot.pdpu.ac.in>
2023-03-04 06:35:08 +0530

    Update and added new models

--- models/campground.js
+++ models/campground.js
@@ -1,22 +1,44 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review');
 
 const CampgroundSchema = new Schema ({
     title : {
-        type : String
+        type : String,
+        required : true
     },
     location : {
-        type : String
+        type : String,
+        required : true
     },
     price : {
-        type : Number
+        type : Number,
+        required : true
     },
     description : {
-        type : String
+        type : String,
+        required : true
     },
     image : {
-        type : String
-    }
+        type : String,
+        required : true
+    },
+    reviews : [
+        {
+            type : Schema.Types.ObjectId,
+            ref : "Review"
+        }
+    ]
 });
+
+CampgroundSchema.post('findOneAndDelete',async function(data){
+    if(data){
+        await Review.remove({
+            _id : {
+                $in: data.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground',CampgroundSchema);
