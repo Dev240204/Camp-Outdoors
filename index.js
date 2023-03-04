581dce25cb57961b04cbecc7b7b1f6348f590f98
Dev <dev.ace21@sot.pdpu.ac.in>
2023-03-04 06:31:57 +0530

    updated

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 const express = require('express');
 const path = require('path');
-const Campground = require('./models/campground');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const ExpressError = require('./Utils/ExpressError')
+const campgrounds = require('./Routes/campgrounds');
+const reviews = require('./Routes/reviews')
+const session = require('express-session');
+const flash = require('connect-flash');
 
+const app = express();
 mongoose.set('strictQuery',true);
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
-    .then(()=>{
-        console.log("Data base Connection Open!!");
+.then(()=>{
+    console.log("Data base Connection Open!!");
     })
     .catch(()=>{
         console.log("Error in Connecting Data Base");
     })
 
-const app = express();
-
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
+
+const sessionConfig = {
+    secret : 'thisisConfig',
+    resave : false,
+    saveUninitialized : true,
+    cookie : {
+        httpOnly : true,
+        expires : Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge : 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(flash())
+
+app.use((req,res,next)=>{
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.use('/campgrounds',campgrounds);
+app.use('/campgrounds/:id/reviews',reviews)
 
 app.engine('ejs', ejsMate);
 app.set('view engine','ejs');
@@ -26,44 +53,17 @@ app.set('views',path.join(__dirname,'views'));
 
 // Home Page
 app.get('/',(req,res)=>{
-    res.render('home');
-})
-//Index page to display all the campgrounds
-app.get('/campgrounds',async (req,res)=>{
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index',{campgrounds});
-})
-// To create new Campground
-app.get('/campgrounds/new',(req,res)=>{
-    res.render('campgrounds/new');
-})
-app.post('/campgrounds',async (req,res)=>{
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`);
+    res.render('campgrounds/home');
 })
 
-// To display details of specific Campground
-app.get('/campgrounds/:id',async (req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show',{campground});
+app.all('*',(req,res,next)=>{
+    next(new ExpressError("Page Not Found",404));
 })
 
-// To edit or Update Campground details
-app.get('/campgrounds/:id/edit',async (req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit',{campground});
-})
-app.put('/campgrounds/:id',async(req,res)=>{
-    const {id} = req.params
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
-    res.redirect(`/campgrounds/${campground._id}`);
-})
-
-app.delete('/campgrounds/:id',async(req,res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
+app.use((err,req,res,next)=>{
+    const {status = 500} = err;
+    if(!err.message) err.message = "Something went wrong";
+    res.status(status).render('error',{err});
 })
 
 app.listen(3000,()=>{
