66d0ea4e2b9061597526830c5b98b1402def64d5
Dev <dev.ace21@sot.pdpu.ac.in>
2023-03-05 11:06:57 +0530

    Update index

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./Utils/ExpressError')
-const campgrounds = require('./Routes/campgrounds');
-const reviews = require('./Routes/reviews')
+const campgroundsRoutes = require('./Routes/campgrounds');
+const reviewsRoutes = require('./Routes/reviews')
+const userRoutes = require('./Routes/users')
 const session = require('express-session');
 const flash = require('connect-flash');
 const User = require('./models/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-
-const app = express();
 
 // Mongoose Setup for Data Base Connection and Error Handling 
 mongoose.set('strictQuery',true);
@@ -24,15 +23,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
         console.log("Error in Connecting Data Base");
     })
 
-// Express Setup for Parsing and Serving Static Files
-app.use(express.urlencoded({extended : true}));
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname,'public')));
+
+const app = express();
 
 // Ejs and Ejs-Mate Setup for Templating
 app.engine('ejs', ejsMate);
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+
+// Express Setup for Parsing and Serving Static Files
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')));
 
 // Session Setup for Flash Messages and Authentication 
 const sessionConfig = {
@@ -48,17 +50,6 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash())
 
-// Middleware for Flash Messages
-app.use((req,res,next)=>{
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})
-
-// Routes for Campgrounds and Reviews
-app.use('/campgrounds',campgrounds);
-app.use('/campgrounds/:id/reviews',reviews)
-
 // Passport Setup for Authentication
 app.use(passport.initialize());
 app.use(passport.session());
@@ -66,6 +57,19 @@ passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// Middleware for Flash Messages
+app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+// Routes for Campgrounds and Reviews
+app.use('/',userRoutes);
+app.use('/campgrounds',campgroundsRoutes);
+app.use('/campgrounds/:id/reviews',reviewsRoutes);
 
 // Home Page
 app.get('/',(req,res)=>{
