8dc05be2ba8a5041c54dba32976478daaf8d66ef
Dev <dev.ace21@sot.pdpu.ac.in>
2023-03-10 18:54:38 +0530

    Update routes

--- Routes/campgrounds.js
+++ Routes/campgrounds.js
@@ -1,24 +1,71 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const WrapAsync = require('../Utils/WrapAsync');
-const campgrounds = require('../connectors/campgrounds');
-const {isLoggedIn, isAuthor, ValidateData} = require('../middleware');
+const WrapAsync = require("../Utils/WrapAsync");
+const campgrounds = require("../connectors/campgrounds");
+const {
+  isLoggedIn,
+  isAuthor,
+  ValidateData,
+} = require("../middleware");
+const multer = require("multer");
+const { storage } = require("../Cloudinary");
+const upload = multer({ storage });
 
 //Index page to display all the campgrounds
-router.get('/',WrapAsync(campgrounds.campgroundIndex));
+router.get(
+  "/",
+  WrapAsync(campgrounds.campgroundIndex)
+);
 
 // To create new Campground
-router.get('/new',isLoggedIn,campgrounds.renderNewForm);
-router.post('/',isLoggedIn,ValidateData,WrapAsync(campgrounds.createCampground));
+router.get(
+  "/new",
+  isLoggedIn,
+  campgrounds.renderNewForm
+);
+router.post(
+  "/",
+  isLoggedIn,
+  ValidateData,
+  upload.array("image"),
+  WrapAsync(campgrounds.createCampground)
+);
+router.post(
+  "/",
+  upload.single("image"),
+  (req, res) => {
+    console.log(req.body, req.file);
+    res.send("Worked!!");
+  }
+);
 
 // To display details of specific Campground
-router.get('/:id',WrapAsync(campgrounds.showCampground));
+router.get(
+  "/:id",
+  WrapAsync(campgrounds.showCampground)
+);
 
 // To edit or Update Campground details
-router.get('/:id/edit',isLoggedIn,isAuthor,WrapAsync(campgrounds.renderEditForm));
-router.put('/:id',isLoggedIn,isAuthor,ValidateData,WrapAsync(campgrounds.updateCampground));
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  WrapAsync(campgrounds.renderEditForm)
+);
+router.put(
+  "/:id",
+  isLoggedIn,
+  isAuthor,
+  ValidateData,
+  WrapAsync(campgrounds.updateCampground)
+);
 
-// To delete a Campground 
-router.delete('/:id',isLoggedIn,isAuthor,WrapAsync(campgrounds.deleteCampground));
+// To delete a Campground
+router.delete(
+  "/:id",
+  isLoggedIn,
+  isAuthor,
+  WrapAsync(campgrounds.deleteCampground)
+);
 
 module.exports = router;
