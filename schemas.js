c052e12e5fdf3f3c290e0800773d5082602b99fe
Dev <dev.ace21@sot.pdpu.ac.in>
2023-03-18 10:46:35 +0530

    Updated Schemas

--- schemas.js
+++ schemas.js
@@ -4,11 +4,11 @@ module.exports.CampgroundSchema = Joi.object({
     campground : Joi.object({
         title : Joi.string().required(),
         price : Joi.number().required().min(0),
-        // image : Joi.string().required(),
         location : Joi.string().required(),
         description : Joi.string().required()
-    })
-}).required()
+    }).required,
+    DeleteImages : Joi.array()
+});
 
 module.exports.ReviewSchema = Joi.object({
     review : Joi.object({
