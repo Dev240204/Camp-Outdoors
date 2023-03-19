b8d9cdc0444270e6f0c8570b7f64303596e213ee
Dev <dev.ace21@sot.pdpu.ac.in>
2023-03-19 12:18:49 +0530

    Update Schemas

--- schemas.js
+++ schemas.js
@@ -1,18 +1,41 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html')
+
+const extension = (Joi) => ({
+    type : 'string',
+    base : Joi.string(),
+    messages : {
+        'string.escapeHTML' : '{{#label}} must not include HTML!'
+    },
+    rules : {
+        escapeHTML : {
+            validate(value,helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags : [],
+                    allowedAttributes : {},
+                });
+                if(clean !== value) return helpers.error('string.escapeHTML',{value})
+                return clean; 
+            }
+        }
+    }
+})
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.CampgroundSchema = Joi.object({
     campground : Joi.object({
-        title : Joi.string().required(),
+        title : Joi.string().required().escapeHTML(),
         price : Joi.number().required().min(0),
-        location : Joi.string().required(),
-        description : Joi.string().required()
-    }).required,
-    DeleteImages : Joi.array()
+        location : Joi.string().required().escapeHTML(),
+        description : Joi.string().required().escapeHTML()
+    }).required(),
+    deleteImages : Joi.array()
 });
 
 module.exports.ReviewSchema = Joi.object({
     review : Joi.object({
-        body : Joi.string().required(),
+        body : Joi.string().required().escapeHTML(),
         rating : Joi.number().required().min(1).max(5),
     }).required()
 }).required()
